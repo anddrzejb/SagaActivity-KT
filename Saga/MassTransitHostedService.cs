c87a9f6d07a7989b659b13d27ea1355d17c677c0
Andrzej Bakun <andrzej.bakun@billennium.com>
2023-09-21 19:06:21 +0200

    2.4. Added saga basic in memory wiring & initialization

---
+++ Saga/MassTransitHostedService.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+
+namespace Saga;
+
+public class MassTransitHostedService : IHostedService
+{
+    readonly IBusControl bus;
+
+    public MassTransitHostedService(IBusControl bus)
+    {
+        this.bus = bus;
+    }
+
+    public async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await bus.StartAsync(cancellationToken);
+    }
+
+    public async Task StopAsync(CancellationToken cancellationToken)
+    {
+        await bus.StopAsync(cancellationToken);
+    }
+}
--- Saga/Program.cs
+++ Saga/Program.cs
@@ -1,3 +1,7 @@
+using MassTransit;
+using Saga;
+using Saga.Configuration;
+using Saga.Events;
 using Serilog;
 using Serilog.Events;
 
@@ -18,10 +22,29 @@ builder.Host.ConfigureLogging((hostingContext, logging) =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddMassTransit(cfg =>
+{
+    cfg.AddSagaStateMachine<SagaStateMachine, SagaStateData>().InMemoryRepository();
+    
+    cfg.UsingInMemory((context, opt) =>
+    {
+        opt.ConfigureEndpoints(context);
+    });
+});
+
+builder.Services.AddHostedService<MassTransitHostedService>();
+
 var app = builder.Build();
 app.UseSwagger();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/saga", async (IPublishEndpoint publishEndpoint) =>
+{
+    await publishEndpoint.Publish(new InitSagaEvent()
+    {
+        CorrelationId = Guid.NewGuid()
+    });
+    return Results.Ok();
+});
 
 app.UseSwaggerUI();
 app.Run();
