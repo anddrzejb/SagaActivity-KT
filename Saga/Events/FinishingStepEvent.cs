b23b67bcc3fbef2913154cb9844cd52db50e7290
Andrzej Bakun <andrzej.bakun@billennium.com>
2023-09-21 18:35:38 +0200

    2.2. Added saga event classes

---
+++ Saga/Events/FinishingStepEvent.cs
@@ -0,0 +1,8 @@
+﻿using MassTransit;
+
+namespace Saga.Events;
+
+public class FinishingStepEvent : CorrelatedBy<Guid>
+{
+    public Guid CorrelationId { get; init; }
+}
---
+++ Saga/Events/InitialSagaEvent.cs
@@ -0,0 +1,8 @@
+﻿using MassTransit;
+
+namespace Saga.Events;
+
+public class InitSagaEvent : CorrelatedBy<Guid>
+{
+    public Guid CorrelationId { get; init; }
+}
