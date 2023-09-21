3895a4aa7fff6cfe648f0954b816e5703afa52de
Andrzej Bakun <andrzej.bakun@billennium.com>
2023-09-21 18:33:30 +0200

    2.1. Added saga state

---
+++ Saga/Configuration/SagaStateData.cs
@@ -0,0 +1,13 @@
+﻿using Automatonymous;
+using MassTransit.Saga;
+
+namespace Saga.Configuration;
+
+public class SagaStateData : SagaStateMachineInstance, ISagaVersion
+{
+    public Guid CorrelationId { get; set; }
+    
+    public string CurrentState { get; set; } = default!;
+    
+    public int Version { get; set; }
+}
